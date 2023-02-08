2cb11abf62f522e15aeaca914374c45545ff5ade
mikatomik <mikec_2007@hotmail.com>
2023-02-08 01:32:42 -0500

    Add dodging mechanic and change_state func

--- player/player.gd
+++ player/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody3D
 @export var walk_speed : int = 5
 @export var walk_accel : int = 10
 @export var fall_accel : int = 3
+@export var dodge_speed : int = 200
+@export var dodge_accel : int = 8
+@export var dodge_jump_strength : int = 8
 @export var gravity : int = -30
 @export var terminal_velocity : int = -40
 @export var jump_strength : int = 15
@@ -11,12 +14,15 @@ extends CharacterBody3D
 var forward_relative_to_camera : Vector3
 var input_dir : Vector3
 var state = IDLE
+var previous_state = IDLE
 
 enum {
 	IDLE,
 	WALKING,
 	JUMPING,
-	FALLING
+	FALLING,
+	STRAFING,
+	DODGING
 }
 
 func _ready():
@@ -33,10 +39,13 @@ func _physics_process(delta):
 			move_and_slide()
 			
 			if input_dir.length() > 0:
-				state = WALKING
+				change_state(WALKING)
 				
 			if Input.is_action_just_pressed("jump") and is_on_floor():
-				state = JUMPING
+				change_state(JUMPING)
+				
+			if Input.is_action_pressed("strafe"):
+				change_state(STRAFING)
 			
 		WALKING:
 			get_directional_input()
@@ -46,13 +55,16 @@ func _physics_process(delta):
 			move_and_slide()
 			
 			if !is_on_floor():
-				state = FALLING
+				change_state(FALLING)
 			
 			if input_dir.length() <= 0:
-				state = IDLE
+				change_state(IDLE)
 				
 			if Input.is_action_just_pressed("jump") and is_on_floor():
-				state = JUMPING
+				change_state(JUMPING)
+				
+			if Input.is_action_pressed("strafe"):
+				change_state(STRAFING)
 				
 		JUMPING:
 			velocity.y = jump_strength
@@ -62,7 +74,7 @@ func _physics_process(delta):
 			interpolate_velocity(walk_speed, fall_accel, delta)
 			move_and_slide()
 			
-			state = FALLING
+			change_state(FALLING)
 			
 		FALLING:
 			get_directional_input()
@@ -72,13 +84,36 @@ func _physics_process(delta):
 			move_and_slide()
 			
 			if is_on_floor() and input_dir.length() > 0:
-				state = WALKING
-			elif is_on_floor() and input_dir.length() <= 0:
-				state = IDLE
+				change_state(WALKING)
 				
-	update_state_readout()
-	
-	
+			elif is_on_floor() and input_dir.length() <= 0:
+				change_state(IDLE)
+				
+			if Input.is_action_pressed("strafe"):
+				change_state(STRAFING)
+				
+		STRAFING:
+			get_directional_input()
+			align_mesh_to_input_dir()
+			apply_gravity(delta)
+			interpolate_velocity(walk_speed, walk_accel, delta)
+			move_and_slide()
+			
+			if Input.is_action_just_released("strafe"):
+				change_state(WALKING)
+				
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				change_state(DODGING)
+				
+		DODGING:
+			get_directional_input()
+			align_mesh_to_input_dir()
+			apply_gravity(delta)
+			interpolate_velocity(dodge_speed, dodge_accel, delta)
+			velocity.y = dodge_jump_strength
+			move_and_slide()
+			change_state(previous_state)
+				
 	
 func get_directional_input():
 	forward_relative_to_camera = -get_node("camera_pivot").basis.z
@@ -103,7 +138,32 @@ func apply_gravity(delta):
 	if velocity.y < terminal_velocity:
 		velocity.y = terminal_velocity
 	
-func update_state_readout():
+func align_mesh_to_input_dir():
+	
+	if state == STRAFING or state == DODGING:
+		var current_rot_quat = Quaternion($body_rig.basis.orthonormalized()).normalized()
+		var desired_rot_quat = Quaternion($camera_pivot.basis.orthonormalized()).normalized()
+		var interpolated_quat = current_rot_quat.slerp(desired_rot_quat, mesh_rot_speed)
+		
+		$body_rig.basis = Basis(interpolated_quat).orthonormalized()
+		
+	else:
+		if input_dir.length() > 0:
+			var input_dir_2d = Vector2(input_dir.x, input_dir.z).normalized()
+			var camera_dir_2d = -Vector2($camera_pivot.basis.z.x, $camera_pivot.basis.z.z).normalized()
+			var angle_to = -camera_dir_2d.angle_to(input_dir_2d)
+			
+			var desired_quat = Quaternion($camera_pivot.basis.rotated(Vector3.UP, angle_to).orthonormalized()).normalized()
+			var current_quat = Quaternion($body_rig.basis.orthonormalized()).normalized()
+			
+			var interpolated_quat = current_quat.slerp(desired_quat, mesh_rot_speed)
+			
+			$body_rig.basis = Basis(interpolated_quat).orthonormalized()
+
+func change_state(new_state):
+	previous_state = state
+	state = new_state
+	
 	if state == 0:
 		$Label3D.text = "IDLE"
 	elif state == 1:
@@ -112,20 +172,11 @@ func update_state_readout():
 		$Label3D.text = "JUMPING"
 	elif state == 3:
 		$Label3D.text = "FALLING"
+	elif state == 4:
+		$Label3D.text = "STRAFING"
+	elif state == 5:
+		$Label3D.text = "DODGING"
 		
 	else:
 		$Label3D.text = "IT'S A MAGIC MYSTERY"
 	
-	
-func align_mesh_to_input_dir():
-	if input_dir.length() > 0:
-		var input_dir_2d = Vector2(input_dir.x, input_dir.z).normalized()
-		var camera_dir_2d = -Vector2($camera_pivot.basis.z.x, $camera_pivot.basis.z.z).normalized()
-		var angle_to = -camera_dir_2d.angle_to(input_dir_2d)
-		
-		var desired_quat = Quaternion($camera_pivot.basis.rotated(Vector3.UP, angle_to).orthonormalized()).normalized()
-		var current_quat = Quaternion($body_rig.basis.orthonormalized()).normalized()
-		
-		var interpolated_quat = current_quat.slerp(desired_quat, mesh_rot_speed)
-		
-		$body_rig.basis = Basis(interpolated_quat).orthonormalized()
